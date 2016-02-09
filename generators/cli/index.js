ad38e34cbc1e6d0d8c6c15cecdd8e0d564ed6352
james young <jmeyoung@gmail.com>
2016-02-09 22:18:28 +0000

    wip

--- generators/cli/index.js
+++ generators/cli/index.js
@@ -8,7 +8,7 @@ module.exports = generators.Base.extend({
         this.fs.copy(this.templatePath('.esdoc.json'), this.destinationPath('.esdoc.json'));
         this.fs.copy(this.templatePath('.eslintignore'), this.destinationPath('.eslintignore'));
         this.fs.copy(this.templatePath('.eslintrc'), this.destinationPath('.eslintrc'));
-        this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
+        this.fs.copy(this.templatePath('_gitignore'), this.destinationPath('.gitignore'));
         this.fs.copy(this.templatePath('.npmrc'), this.destinationPath('.npmrc'));
         this.fs.copy(this.templatePath('.nvmrc'), this.destinationPath('.nvmrc'));
 
